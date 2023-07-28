ab9e3247576979ada1039b0d9f508c9f768bd162
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2023-07-28 23:40:38 +0100

    Refresh ladder

---
+++ app/add_account_to_player.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+import csv
+from riotwatcher import LolWatcher, ApiError
+
+from tempfile import NamedTemporaryFile
+import shutil
+
+load_dotenv()
+api_token = os.environ.get("API_KEY")
+lol_watcher = LolWatcher(api_token)
+region = 'EUW1'
+
+if __name__ == "__main__":
+    name = input("Enter their player name: ")
+
+    usernames = []
+    finished = False
+    while not finished:
+        username = input("Enter a username (type 'done' if finished): ")
+        if username == "done":
+            if len(usernames) > 0:
+                finished = True
+            else:
+                print("No username has been entered yet")
+        else:
+            print("Added.")
+            usernames.append(username)
+
+    puuids = []
+    for username in usernames:
+        response = lol_watcher.summoner.by_name(region, username)
+        puuids.append(response['puuid'])
+
+    temp_file = NamedTemporaryFile(mode='w', newline='', delete=False)
+    with open('app/players.csv', 'r', newline='', encoding="utf-8") as players_file, temp_file:
+        reader = csv.reader(players_file)
+        writer = csv.writer(temp_file)
+
+        for row in reader:
+            if row[0] == name:
+                writer.writerow(row + puuids)
+            else:
+                writer.writerow(row)
+    shutil.move(temp_file.name, 'app/players.csv')
