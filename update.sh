879aaaefc95f84053065fb6a0350c398b19084a7
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2024-01-12 22:31:16 +0000

    Add refresh script

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/arcane/repos/ilol-ladder/venv/bin/activate
+
+# Run the Python script
+python /home/arcane/repos/ilol-ladder/app/main.py &
+python_pid=$!
+
+# Wait for the Python script to finish executing
+wait $python_pid
+
+# Check for errors in the Python script
+if [ $? -eq 0 ]; then
+    # Add all changed files
+    git add .
+
+    # Commit changes with the message "Refresh ladder"
+    git commit -m "Refresh ladder"
+
+    # Push changes to the remote repository
+    git push
+else
+    echo "Python script failed to execute."
+fi
+
+deactivate
